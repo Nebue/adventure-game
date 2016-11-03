6bec3b84d4025175f5e2616000e8839d5960af15
MSI\Nebue Yemane <99039746@mydavinci.nl>
2016-11-03 09:47:13 +0100

    advanture game

---
+++ game.js
@@ -0,0 +1,9 @@
+function Begin(){
+	console.log('start');
+	document.getElementById('start').style.display = "none";
+	document.getElementById('level_title').innerHTML ="";
+	document.getElementById('text').innerHTML ="";
+	document.body.style.backgroundImage = "url('unnamed.png')";
+	document.body.style.backgroundSize = "1750px 820px";
+
+}
