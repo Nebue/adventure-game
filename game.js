e6d3b0e5abf54c60d42e3193737fd6bb7e716ba6
MSI\Nebue Yemane <99039746@mydavinci.nl>
2016-11-28 14:25:13 +0100

    versie 5.0

--- game.js
+++ game.js
@@ -3,7 +3,6 @@ function Begin(){
 	document.getElementById('start').style.display = "none";
 	document.getElementById('level_title').innerHTML ="";
 	document.getElementById('text').innerHTML =" Hallo, ik ben Dora. Waar kan je de verassingsheuvel vinden?";
-
 	document.getElementById('text').style.top = "200px";
 	document.getElementById('text').style.position = "absolute";
 	document.getElementById('text').style.left = "-100px";
@@ -37,17 +36,13 @@ function brug(){
 	
 	document.getElementById('house').style.display = "none";
 		document.getElementById('heuvel').style.display = "none";
+		document.getElementById('next').style.display= "none";
 	
-
-
-	
-
 }
 
 
 var auto_interval
 function showtekst (tekst){
-	document.getElementById('brug').innerHTML = "";
 	var auto_char = tekst.split('');
 	var auto_length = auto_char.length - 1;
 	var auto_index = 0;
@@ -63,8 +58,47 @@ function showtekst (tekst){
 }
 
 function myFunction() {
-    location.reload();
+    	console.log('start');
+    	clearInterval(auto_interval);
+	document.getElementById('start').style.display = "none";
+	document.getElementById('level_title').innerHTML ="";
+	document.getElementById('text').innerHTML =" Hallo, ik ben Dora. Waar kan je de verassingsheuvel vinden?";
+	document.getElementById('text').style.top = "200px";
+	document.getElementById('text').style.position = "absolute";
+	document.getElementById('text').style.left = "-100px";
+	document.getElementById('text').style.textShadow = "none";
+	document.getElementById('text').style.color = "white";
+	document.body.style.backgroundImage = "url('unnamed.png')";
+	document.body.style.backgroundSize = "1540px 760px";
+	document.getElementById('refresh').style.display = "none";
+document.getElementById('brug').style.display = "inline";
+	document.getElementById('house').style.display = "inline";
+	document.getElementById('heuvel').style.display = "inline";
+	document.getElementById('key').style.display = "none";
+	document.getElementById('over').style.display = "none";
+	document.getElementById('next').style.display= "none";
 }
 
+
+function Huis(){
+showtekst('Kan jij de sleutel van het huis in het bos vinden?');
+document.body.style.backgroundImage = "url('hetbos.jpg')";
+document.getElementById('house').style.display = "none";
+		document.getElementById('heuvel').style.display = "none";
+		document.getElementById('brug').style.display = "none";
+		document.getElementById('text').innerHTML = "";
+		document.getElementById('text').style.textShadow = "2px 2px black";
+		document.getElementById('text').style.position = "absolute";
+		document.getElementById('text').style.top = "690px";
+		document.getElementById('text').style.left = "0px";
+		document.getElementById('key').style.display = "inline";
+		document.getElementById('key').style.position = "absolute";
+		document.getElementById('key').style.left = "1400px";
+		document.getElementById('key').style.top = "250px";
+document.getElementById('refresh').style.display = "inline";
+	document.getElementById('refresh').style.fontSize = "20px";
+	document.getElementById('next').style.display= "inline";
+
+}
 
 
