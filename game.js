a7452aa3c82cf8a8a7aff395464b31e4cac96901
MSI\Nebue Yemane <99039746@mydavinci.nl>
2016-12-01 12:07:01 +0100

    versie 6.0

--- game.js
+++ game.js
@@ -11,32 +11,42 @@ function Begin(){
 	document.body.style.backgroundImage = "url('unnamed.png')";
 	document.body.style.backgroundSize = "1540px 760px";
 	document.getElementById('refresh').style.display = "none";
-document.getElementById('brug').style.display = "inline";
+	document.getElementById('brug').style.display = "inline";
 	document.getElementById('house').style.display = "inline";
 	document.getElementById('heuvel').style.display = "inline";
+	document.getElementById('over').style.display = "none";
+	document.getElementById('next1').style.display= "none";
+	document.getElementById('stelen').style.display = "none";
+
+
+
 
 }
 
 
 function brug(){
 		clearInterval(auto_interval);
-	document.getElementById('text').innerHTML = "";
-	showtekst('Oneeeee Boots, Troll Booth doet de poort dicht. We kunnen helaas niet verder :(');
-	document.body.style.backgroundImage = "url('brug.jpg')";
-	document.getElementById('brug').style.display = "none";
-	document.getElementById('text').style.top = "680px";
-	document.getElementById('text').style.textShadow = "2px 2px black";
-
-	document.getElementById('refresh').style.display = "inline";
-	document.getElementById('refresh').style.fontSize = "20px";
-	document.getElementById('over').style.display = "inline";
-	document.getElementById('over').style.position = "absolute";
-	document.getElementById('over').style.left = "540px";
-	document.getElementById('over').style.top= "10px";
-	
-	document.getElementById('house').style.display = "none";
+		document.getElementById('text').innerHTML = "";
+		showtekst('Oneeeee Boots, Troll Booth doet de poort dicht. We kunnen helaas niet verder :(');
+		document.body.style.backgroundImage = "url('brug.jpg')";
+		document.getElementById('brug').style.display = "none";
+		document.getElementById('text').style.top = "680px";
+		document.getElementById('text').style.textShadow = "2px 2px black";
+		document.getElementById('refresh').style.display = "inline";
+		document.getElementById('refresh').style.fontSize = "20px";
+		document.getElementById('over').style.display = "inline";
+		document.getElementById('over').style.position = "absolute";
+		document.getElementById('over').style.left = "540px";
+		document.getElementById('over').style.top= "10px";
+		document.getElementById('house').style.display = "none";
 		document.getElementById('heuvel').style.display = "none";
 		document.getElementById('next').style.display= "none";
+		document.getElementById('key').style.display= "none";
+		document.getElementById('text').style.display = "inline";
+		document.getElementById('over').style.display = "none";
+		    document.getElementById('swieber').style.display = "none";
+		    	document.getElementById('next1').style.display= "none";
+		    		document.getElementById('stelen').style.display = "none";
 	
 }
 
@@ -54,7 +64,7 @@ function showtekst (tekst){
 		else {
 			clearInterval(auto_interval);
 		}
-	},50);
+	},38);
 }
 
 function myFunction() {
@@ -71,34 +81,89 @@ function myFunction() {
 	document.body.style.backgroundImage = "url('unnamed.png')";
 	document.body.style.backgroundSize = "1540px 760px";
 	document.getElementById('refresh').style.display = "none";
-document.getElementById('brug').style.display = "inline";
+	document.getElementById('brug').style.display = "inline";
 	document.getElementById('house').style.display = "inline";
 	document.getElementById('heuvel').style.display = "inline";
 	document.getElementById('key').style.display = "none";
-	document.getElementById('over').style.display = "none";
+
 	document.getElementById('next').style.display= "none";
+	document.getElementById('key').style.display= "none";
+	document.getElementById('text').style.display = "inline";
+	 document.getElementById('swieber').style.display = "none";
+	 	document.getElementById('next1').style.display= "none";
+	 		document.getElementById('stelen').style.display = "none";
 }
 
 
 function Huis(){
-showtekst('Kan jij de sleutel van het huis in het bos vinden?');
-document.body.style.backgroundImage = "url('hetbos.jpg')";
-document.getElementById('house').style.display = "none";
+	clearInterval(auto_interval);
+		showtekst('Kan jij de sleutel van het huis in het bos vinden?');
+		document.body.style.backgroundImage = "url('hetbos.jpg')";
+		document.getElementById('house').style.display = "none";
 		document.getElementById('heuvel').style.display = "none";
 		document.getElementById('brug').style.display = "none";
 		document.getElementById('text').innerHTML = "";
 		document.getElementById('text').style.textShadow = "2px 2px black";
 		document.getElementById('text').style.position = "absolute";
-		document.getElementById('text').style.top = "690px";
-		document.getElementById('text').style.left = "0px";
+		document.getElementById('text').style.top = "650px";
+		document.getElementById('text').style.right = "200px";
 		document.getElementById('key').style.display = "inline";
 		document.getElementById('key').style.position = "absolute";
-		document.getElementById('key').style.left = "1400px";
+		document.getElementById('key').style.left = "300px";
 		document.getElementById('key').style.top = "250px";
-document.getElementById('refresh').style.display = "inline";
-	document.getElementById('refresh').style.fontSize = "20px";
-	document.getElementById('next').style.display= "inline";
+     	document.getElementById('refresh').style.display = "inline";
+		document.getElementById('refresh').style.fontSize = "20px";
+		document.getElementById('next').style.display= "none";
+		document.getElementById('key').style.display= "inline";
+		document.getElementById('text').style.display = "inline";
+		 document.getElementById('swieber').style.display = "none";
+		 	document.getElementById('next1').style.display= "none";
+			document.getElementById('stelen').style.display = "none";
+
+}
+
+function sleutel() {
+	clearInterval(auto_interval);
+    alert("Gefeliciteerd je hebt de sleutel gevonden!");
+    document.getElementById('next').style.display= "inline";
+    document.getElementById('text').style.display = "inline";
+    showtekst(' Goedzo ! Je hebt de sleutel gevonden je kan je weg vervolgen.');
+    document.getElementById('swieber').style.display = "none";
+    	document.getElementById('next1').style.display= "none";
+    		document.getElementById('stelen').style.display = "none";
+   
+
+}
+
+function volgende(){
+	clearInterval(auto_interval);
+document.getElementById('text').style.display = "inline";
+document.body.style.backgroundImage = "url('bootssleutel.jpg')";
+document.getElementById('key').style.display= "none";
+document.getElementById('next').style.display= "none";
+document.getElementById('next1').style.display= "inline";
+document.getElementById('swieber').style.display = "inline";
+showtekst(' Daar hebben we Zwieber de vos. Pas op Boots hij wilt de sleutel stelen. We moeten ons verstoppen! ');
+	document.getElementById('stelen').style.display = "none";
+
 
 }
 
 
+function volgende1(){
+	clearInterval(auto_interval);
+
+document.body.style.backgroundImage = "url('swieberdora.jpg')";
+document.getElementById('swieber').style.display = "none";
+document.getElementById('text').style.display = "none";
+	document.getElementById('stelen').style.display = "none";
+
+
+}
+
+
+function niet(){
+	clearInterval(auto_interval);
+	showtekst('Zwieber niet stelen!!!!')
+		document.getElementById('stelen').style.display = "inline";
+}
